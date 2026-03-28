f1288f2718325aa7af10be88fcd95268ffdd45f1
Hoaison3108 <163942895+Hoaison3108@users.noreply.github.com>
2026-03-28 15:11:32 +0700

    feat: add team members data structure to team.ts

--- src/data/team.ts
+++ src/data/team.ts
@@ -2,21 +2,21 @@ export const teamMembers = [
   {
     name: "Trần Công H",
     role: "Tổng Giám Đốc",
-    image: "https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?q=80&w=1000&auto=format&fit=crop",
+    image: "https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Lê Thanh C",
     role: "Phó Tổng Giám Đốc",
-    image: "https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?q=80&w=1000&auto=format&fit=crop",
+    image: "https://images.unsplash.com/photo-1741455620227-3b1c51e01419?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Nguyễn Trọng S",
     role: "GĐ. Điều Hành Mỏ",
-    image: "https://images.unsplash.com/photo-1531834685032-c34bf0d84c77?q=80&w=1000&auto=format&fit=crop",
+    image: "https://images.unsplash.com/photo-1648474484044-bb82df2f5a1f?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Trần Minh C",
     role: "TP. Kinh Doanh",
-    image: "https://images.unsplash.com/photo-1541888946425-d81bb19480c5?q=80&w=1000&auto=format&fit=crop",
+    image: "https://images.unsplash.com/photo-1757406005026-990a2b022e49?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   }
 ];
